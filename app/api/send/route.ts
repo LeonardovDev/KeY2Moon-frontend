204062fd20118f54d69e4990486747e32eae3bf5
LeonardovDev <leonardov0529@gmail.com>
2024-06-18 14:48:21 -0700

    fix:build error 37

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -3,7 +3,8 @@ import type { NextRequest, NextResponse } from "next/server";
 import { EmailTemplate } from "@/components/EmailTemplate";
 import { Resend } from "resend";
 
-const resend = new Resend("re_M7Kq6jL4_KgMZUCP7khz7RBYT18Z3SDm3");
+// const resend = new Resend("re_M7Kq6jL4_KgMZUCP7khz7RBYT18Z3SDm3");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const { message } = await req.json();
